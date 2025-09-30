28ce49e9c8e12f0eafaa924f4586ae17fecfed1b
Ramazan Sahin <ramazansahin968@gmail.com>
2025-09-30 12:06:53 +0300

    Refactor main app structure and add HomePage widget; include flutter_staggered_grid_view dependency

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
